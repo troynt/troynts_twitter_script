3d58fe6af2506eff0654a2f2a58d392701cb7317
Troy Thompson <troynt@gmail.com>
2010-05-11 18:49:53 -0400

    version bump, getting rid of scrollbars

--- troynts_twitter_script.user.js
+++ troynts_twitter_script.user.js
@@ -3,7 +3,7 @@ scr_meta=<><![CDATA[
 // @name    @troynt's Twitter Script
 // @namespace  http://twitter.com/troynt
 // @description  Nested Replies, RT button, Custom Search Tabs, YouTube Embed, TwitPic Embed, URL Expansion, Hash Tag Search Links
-// @version    11.5
+// @version    11.6
 // @include    http://twitter.com*
 // @include    http://www.twitter.com*
 // @include    https://twitter.com*
@@ -421,7 +421,7 @@ tnt_twitter = {
     /* highlight tweets to you */
     var css = '.hentry.to_me { background:#ffe; }';
     
-    css += '.hentry { clear:both; overflow:auto; }';
+    css += '.hentry { clear:both; }';
     
     /* media embed */
     css += '.tnt-image { float:right; vertical-align:middle; }';
